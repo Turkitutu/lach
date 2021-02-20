aa902dad015e432f9fe2b76b9064fe962460b074
Turkitutu <turkitututfm@gmail.com>
2021-02-20 13:02:02 +0100

    remove comments

--- src/components/FixedBtns.vue
+++ src/components/FixedBtns.vue
@@ -1,23 +1,4 @@
 <template>
-  <!--<div class="text-center">
-    <v-btn v-show="true" fab color="grey darken-4" fixed large right bottom>
-      <v-icon color="white">mdi-email</v-icon>
-    </v-btn>
-    <v-btn
-      v-scroll="onScroll"
-      v-show="fab"
-      fab
-      dark
-      fixed
-      bottom
-      right
-      color="primary"
-      @click="toTop"
-    >
-      <v-icon white large>mdi-chevron-up</v-icon>
-    </v-btn>
-  </div>-->
-
   <v-col cols="12" sm="6">
     <v-btn
       v-scroll="onScroll"
